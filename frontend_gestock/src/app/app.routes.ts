05d8f6fb4f76e2bf78117f9d4d7d3290068d3bf5
Santiago Ruiz <ericksantiago7p@gmail.com>
2026-08-13 16:11:39 -0500

    resolviendo conflictos

--- frontend_gestock/src/app/app.routes.ts
+++ frontend_gestock/src/app/app.routes.ts
@@ -1,21 +1,57 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login/login';
+import { CreacionComponent } from './login/creacion/creacion';
 import { LayoutComponent } from './layout/layout/layout';
-import { ReportesComponent } from './pages/reportes/reportes';
+import { EmpresasComponent } from './pages/empresas/empresas';
+import { PanelComponent } from './pages/panel/panel';
+import { CrearInventarioComponent } from './pages/crear-inventario/crear-inventario';
 
 export const routes: Routes = [
-    {
+  // Redirección inicial a /app si se entra a la raíz
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full'
+  },
+
+  // Rutas públicas / Login
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'creacion-usuarios',
+    component: CreacionComponent
+  },
+
+  // Layout Contenedor con sus Vistas Hijas
+  {
+    path: 'app',
+    component: LayoutComponent,
+    children: [
+      {
         path: '',
-        component: LayoutComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: 'reportes',
-                pathMatch: 'full'
-            },
-            {
-                path: 'reportes',
-                component: ReportesComponent
-            }
-        ]
-    }
+        redirectTo: 'empresas',
+        pathMatch: 'full'
+      },
+      {
+        path: 'empresas',
+        component: EmpresasComponent
+      },
+      {
+        path: 'panel',
+        component: PanelComponent
+      },
+      {
+        path: 'crear-inventario',
+        component: CrearInventarioComponent
+      }
+    ]
+  },
+
+  // Redirección comodín
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
